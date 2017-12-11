aa26a7846a2e9995c2c248185ddab00c97341a44
Andrey Kasatkin <a@svetliy.com>
2017-12-11 10:27:47 -0500

    add zip for a version 0.5.0

--- zype.php
+++ zype.php
@@ -8,7 +8,7 @@ if (!class_exists('Themosis')) {
  * Plugin Name: Zype
  * Plugin URI: https://www.zype.com/
  * Description: Plugin for integration with the platform Zype.com. Using the plugin you can sell subscriptions to media content keep statistics on video views and audio, create playlists and insert video with shortcode and to display the live broadcast.
- * Version: 1.0.0
+ * Version: 0.5.0
  * Author: Zype
  * Author URI: http://zype.com/
  * Text Domain: plugin-textdomain.
@@ -36,7 +36,7 @@ defined('DS') ? DS : define('DS', DIRECTORY_SEPARATOR);
  */
 defined('ZYPE_MEDIA') ? ZYPE_MEDIA : define('ZYPE_MEDIA', 'plugin-textdomain');
 
-define('ZYPE_WP_VERSION', '1.0');
+define('ZYPE_WP_VERSION', '0.5.0');
 define('ZYPE_WP_OPTIONS', 'zype_wp');
 
 /*
