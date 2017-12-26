15874e19eae925ed36f4f8d88056a61658661102
Andrey Kasatkin <a@svetliy.com>
2017-12-26 12:46:57 -0500

    add more description to admin pages

--- resources/views/admin/braintree.php
+++ resources/views/admin/braintree.php
@@ -7,7 +7,16 @@ $plans = \Zype::get_all_plans();
 
 <div class="wrap zype-admin">
 	<h2><?php echo get_admin_page_title();?></h2>
-    <p>Select all plans that you would like to enable on a website for purchase. If you don't see available plan, please add new one on Zype platform. </p>
+    <p>Any active <a href ="https://admin.zype.com/plans" taraget="_blank">subscription 
+    plans created in your Zype account</a> can be made available for 
+    purchase on your WordPress website. 
+    Simply click below on each plan you would like to feature for sale on
+     any subscription video content on your website. (Note, you may select multiple 
+     plans by cmd+click / ctrl+click on multiple selections.)</p>
+    <p>You can <a href="https://admin.zype.com/plans/new" target="_blank">easily create additional 
+    subscription plans</a> in Zype 
+    if you don’t see an appropriate one below.
+    </p>
 	<form method="post" action="<?php echo admin_url('admin.php');?>">
         <input type="hidden" name="action" value="zype_braintree"/>
         <?php wp_nonce_field('zype_braintree');?>
