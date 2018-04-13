292f31864407f95d7dd508ba59a17ca0ca6fba2e
Chris Smith <imchrissmith@gmail.com>
2018-04-13 16:55:50 -0400

    cleaned up reset password email

--- resources/views/email/forgot_password.php
+++ resources/views/email/forgot_password.php
@@ -1,9 +1,5 @@
 <h3>Password Reset</h3>
-<p>Beavis got your password? Click the following link to reset.</p>
+<p>You recently requested a password reset. Please click the following link to reset your password.</p>
 <p><a href="<?php zype_url('profile');?>/reset-password/<?php echo $dictionary['password_token']; ?>/"><?php zype_url('profile');?>/reset-password/<?php echo $dictionary['password_token']; ?>/</a></p>
 <p></p>
-<p>If you did not request a password reset please disregard this email. If you suspect illicit activity, please contact <a href="mailto:apps@zype.com">support</a>.</p>
-<p></p>
-<p>
-  Thanks for watching<br>
-</p>
+<p>If you did not request a password reset please disregard this email. Thanks for watching!</p>
