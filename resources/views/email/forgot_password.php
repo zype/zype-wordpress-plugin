6b8220c76c7583d78367896a63876fc2e0029b43
Chris Smith <imchrissmith@gmail.com>
2018-04-13 17:01:47 -0400

    Update forgot_password.php

--- resources/views/email/forgot_password.php
+++ resources/views/email/forgot_password.php
@@ -1,5 +1,5 @@
-<h3>Password Reset</h3>
-<p>You recently requested a password reset. Please click the following link to reset your password.</p>
+<h3>Forgot Your Password?</h3>
+<p>We received a request to reset your password. Please use the following link to set a new password for your account.</p>
 <p><a href="<?php zype_url('profile');?>/reset-password/<?php echo $dictionary['password_token']; ?>/"><?php zype_url('profile');?>/reset-password/<?php echo $dictionary['password_token']; ?>/</a></p>
 <p></p>
 <p>If you did not request a password reset please disregard this email. Thanks for watching!</p>
