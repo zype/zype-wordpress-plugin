70c4c91fc943b98e9cba5165d3c43bb597bcb475
Chris Smith <imchrissmith@gmail.com>
2018-04-16 14:04:02 -0400

    Update new_subscription.php

--- resources/views/email/new_subscription.php
+++ resources/views/email/new_subscription.php
@@ -1,12 +1,12 @@
 <div>
-  <h3>New Subscription</h3>
-  <p>Thank you for subscribing to channel, we know you'll enjoy it!</p>
+  <h3>New Subscription Confirmation</h3>
+  <p>Thank you for subscribing, we hope you'll enjoy it!</p>
   <p>You can log in with the email address and password you provided at the following URL: </p>
   <p><a href="<?php zype_url('login'); ?>/"><?php zype_url('login'); ?>/</a></p>
   <p></p>
 
   <p></p>
   <p>
-    Thanks again
+    Thanks again!
   </p>
 </div>
