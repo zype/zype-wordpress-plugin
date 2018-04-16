360fb83ca3960314ee1015c0499f56e52afe52a0
Chris Smith <imchrissmith@gmail.com>
2018-04-16 14:00:02 -0400

    Update new_account.php

--- resources/views/email/new_account.php
+++ resources/views/email/new_account.php
@@ -1,9 +1,9 @@
 <div>
-  <h3>Thank you for signing up for channel</h3>
-  <p>You can log in with the email address and password you provided at the following URL: </p>
+  <h3>Thank you for signing up!</h3>
+  <p>You can log in at the following URL using the email address and password you provided during account creation: </p>
   <p><a href="<?php zype_url('login'); ?>/"><?php zype_url('login'); ?>/</a></p>
   <p></p>
   <p>
-    Thanks again
+    Thanks again!
   </p>
 </div>
