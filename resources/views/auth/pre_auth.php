80766436a16f7c35551b5c612e7ed20a6da19101
Luis <lcarella@zype.com>
2018-05-07 11:15:37 -0300

    Fixes leftover short tags

--- resources/views/auth/pre_auth.php
+++ resources/views/auth/pre_auth.php
@@ -3,6 +3,6 @@
 <main class="site-main">
     <div class="hentry zype-auth-wrap">
         <!-- <div class="entry-header">
-            <h1 class="entry-title"><?= $title ?></h1>
+            <h1 class="entry-title"><?php echo $title ?></h1>
         </div> -->
         <div class="entry-content">
