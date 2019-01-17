7415b521afeebae05968b3bf5229487daf35b316
Mariano Matayoshi <matayoshi.mariano@gmail.com>
2019-01-17 09:33:38 +0900

    CORE 3024 - Fix Tiered plans purchase

--- resources/lib/Zype/Api/Video.php
+++ resources/lib/Zype/Api/Video.php
@@ -10,4 +10,10 @@ class Video extends \Api
     use \ApiOperations\Update;
 
     const RESOURCE_PATH = 'videos';
+
+    public static function entitled($id, $access_token)
+    {
+        $path = self::get_path($id) . '/entitled';
+        return parent::request("GET", $path, ['access_token' => $access_token], false, true);
+    }
 }
