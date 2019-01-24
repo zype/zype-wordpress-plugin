c04189002f1e5a4f9716117593e0453cec8448b0
Mariano Matayoshi <matayoshi.mariano@gmail.com>
2019-01-24 09:36:01 +0900

    CORE 3053 - Add ZypeMedia\Controllers\Admin\ to autoload

--- resources/config/loading.config.php
+++ resources/config/loading.config.php
@@ -7,6 +7,7 @@ return [
     'ZypeMedia\\Providers\\' => themosis_path('plugin.zypemedia.resources') . 'providers',
     'ZypeMedia\\Controllers\\' => themosis_path('plugin.zypemedia.resources') . 'controllers',
     'ZypeMedia\\Services\\' => themosis_path('plugin.zypemedia.resources') . 'services',
+    'ZypeMedia\\Controllers\\Admin\\' => themosis_path('plugin.zypemedia.resources') . 'controllers/admin',
     'ZypeMedia\\Models\\' => themosis_path('plugin.zypemedia.resources') . 'models',
     'ZypeMedia\\Validators\\' => themosis_path('plugin.zypemedia.resources') . 'validators',
 ];
