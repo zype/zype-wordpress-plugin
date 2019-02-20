5e65daa7867fdc092bd15fba9dcc8900eb3e8d5d
Mariano Matayoshi <matayoshi.mariano@gmail.com>
2019-02-20 09:18:07 +0900

    CORE 3196 - Change Logout btn class name on the profile view

--- resources/views/auth/change_password.php
+++ resources/views/auth/change_password.php
@@ -38,7 +38,7 @@
                                 <span class="text">Link Device</span>
                             </a>
                         </li>
-                        <li class="link-device">
+                        <li class="log-out">
                             <a href="<?php echo home_url(\Config::get('zype.logout_url')) ?>/">
                                 <span class="ico"><i class="fa fa-fw fa-sign-out"></i></span>
                                 <span class="text">Logout</span>
